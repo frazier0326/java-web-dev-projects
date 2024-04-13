2e152cd4d05f7ade62d8576bcc5dcd36e204db14
frazier0326 <frazier0326@gmail.com>
2024-04-13 00:09:00 -0500

    updated

--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,8 @@
+
+
 public class StudentPractice {
     public static void main(String[] args){
         //insantiate your Student class below
+        Student student = new Student("Sharease", 12345, 1, 4.0);
     }
 }
